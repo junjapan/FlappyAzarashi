c80c825d7272231a6a7ac42959ec9075f5fe1b6b
jun <watanabejapan2trap@gmail.com>
2022-02-15 14:08:08 +0900

    p380まで作成

---
+++ Assets/Scripts/AzarashiController.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AzarashiController : MonoBehaviour
+{
+    Rigidbody2D rb2d;
+    Animator animator;
+    float angle;
+    bool isDead;
+
+    public float maxHeight;
+    public float flapVelocity;
+    public float relativeVelocityX;
+    public GameObject sprite;
+
+    public bool IsDead()
+    {
+        return isDead;
+    }
+
+    private void Awake()
+    {
+        //自分自身の情報のみ参照できる。startは違う。順番としては次の通り。Awake ⇒ Start ⇒ Update
+        rb2d = GetComponent<Rigidbody2D>();
+        animator = sprite.GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1") && transform.position.y < maxHeight)
+        {
+            Flap();
+        }
+
+        ApplyAngle();
+
+        animator.SetBool("flap", angle >= 0.0f && !isDead);
+    }
+
+    public void Flap()
+    {
+        if (isDead)
+        {
+            return;
+        }
+        rb2d.velocity = new Vector2(0.0f, flapVelocity);
+    }
+
+    void ApplyAngle()
+    {
+        float targetAngle;
+        if (isDead)
+        {
+            targetAngle = 180.0f;
+        }
+        else
+        {
+            //逆三角関数。アークタンジェント。通常は角度から２辺の比率だが、比率から角度を求める。
+            //Rad2Deg　ラジアンからデグリーに変換。
+            targetAngle = Mathf.Atan2(rb2d.velocity.y, relativeVelocityX) * Mathf.Rad2Deg;
+
+        }
+        //線形補間。第１引数と第２引数の差を第３引数の割合で返す
+        angle = Mathf.Lerp(angle, targetAngle, Time.deltaTime * 10.0f);
+
+        sprite.transform.localRotation = Quaternion.Euler(0.0f, 0.0f, angle);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (isDead)
+        {
+            return;
+        }
+        isDead = true;
+    }
+}
---
+++ Assets/Scripts/Block.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Block : MonoBehaviour
+{
+    public float minHeight;
+    public float maxHeight;
+    public GameObject root;
+    public GameObject BottomBlock;
+    // Start is called before the first frame update
+    void Start()
+    {
+        ChangeHeight();
+    }
+
+    void ChangeHeight()
+    {
+        float height = Random.Range(minHeight, maxHeight);
+        root.transform.localPosition = new Vector3(0f, height, 0f);
+        float height2 = Random.Range(-3f, -1f);
+        BottomBlock.transform.localPosition = new Vector3(0f, height2, 0f);
+    }
+
+    void OnScrollEnd()
+    {
+        ChangeHeight();
+    }
+
+}
